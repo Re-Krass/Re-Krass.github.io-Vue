f5e8878d26d39363971d23f58b5ee2e5df7dc920
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2019-09-11 22:41:40 +0200

    refactor(router): add datenschutzerklaerung and impressum

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Datenschutzerklaerung from "./views/Datenschutzerklaerung.vue";
+import Impressum from "./views/Impressum.vue";
 
 Vue.use(Router);
 
@@ -12,6 +14,16 @@ export default new Router({
             path: "/",
             name: "Re-Krass Homepage",
             component: Home
+        },
+        {
+            path: "/datenschutzerklaerung",
+            name: "Datenschutzerklärung",
+            component: Datenschutzerklaerung
+        },
+        {
+            path: "/impressum",
+            name: "Impressum",
+            component: Impressum
         }
     ]
 });
