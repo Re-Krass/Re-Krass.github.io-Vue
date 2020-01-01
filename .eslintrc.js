98e15c85a72b057bfa934156e722801c6b2b4fe7
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2020-01-01 19:49:15 +0100

    refactor(.eslintrc): add lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/essential",
-    "@vue/prettier",
-    // "plugin:vue/strongly-recommended",
-    // "eslint:recommended",
-    // "plugin:prettier/recommended"
-  ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  plugins: ["pug"],
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "@vue/prettier"
+        // "plugin:vue/strongly-recommended",
+        // "eslint:recommended",
+        // "plugin:prettier/recommended"
+    ],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    plugins: ["pug"],
+    overrides: [
+        {
+            files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+            env: {
+                jest: true
+            }
+        }
+    ]
 };
