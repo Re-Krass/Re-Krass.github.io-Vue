07552ebae1bd03162dda5ee623ad3bd7ea776fcf
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2020-01-01 19:57:34 +0100

    refactor(main): add vue settings

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index";
+import store from "./store/index";
+
 import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
+import VueHighlightJS from "vue-highlightjs";
+
+// import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
+
+import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
 
+Vue.use(VueHighlightJS);
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount("#app");
