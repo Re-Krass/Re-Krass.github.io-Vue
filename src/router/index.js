6d7c695da48403dc43669d37b8d93f5fd56df745
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2020-01-01 19:58:08 +0100

    refactor(router): fix routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,39 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Datenschutzerklaerung from "../views/Datenschutzerklaerung.vue";
+import Impressum from "../views/Impressum.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: "/",
+        name: "Re-Krass Homepage",
+        component: Home
+    },
+    {
+        path: "/datenschutzerklaerung",
+        name: "Datenschutzerklärung",
+        component: Datenschutzerklaerung
+    },
+    {
+        path: "/impressum",
+        name: "Impressum",
+        component: Impressum
+    }
+    // {
+    //     path: "/about",
+    //     name: "about",
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+    // }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
